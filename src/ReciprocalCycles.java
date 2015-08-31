c2cae6dce416b0144c5fa1411e39d4aa15dfd86f
Brian Yang <brian.yang328@gmail.com>
2015-08-31 10:17:26 -0400

    Solve Reciprocal Cycles #26

---
+++ src/ReciprocalCycles.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+class ReciprocalCycles {
+	public static void main(String[] args) {
+		int answer = 0;
+		int repeat = 0;
+
+		for (int i = 1; i < 1000; i++) {
+			if (divide(i) > repeat) {
+				answer = i;
+				repeat = divide(i);
+			}
+		}
+
+		System.out.println(answer);
+	}
+
+	public static int divide(int div) {
+		int dividend;
+
+		if (div < 10) {
+			dividend = 10;
+		} else {
+			dividend = 100;
+		}
+
+		ArrayList<Integer> dividends = new ArrayList<Integer>();
+		Integer divisor =  new Integer(div);
+
+		while (dividend != 0) {
+			if (dividend / divisor.intValue() == 0) {
+				dividend = dividend * 10;
+			}
+			dividend -= (dividend / divisor.intValue()) * divisor.intValue();
+			if (dividends.contains(dividend)) {
+				break;
+			}
+			dividends.add(dividend);
+		}
+		return dividends.size() - dividends.indexOf(dividend);
+	}
+}
