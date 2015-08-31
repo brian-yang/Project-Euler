2ec9b44b2063ace6a6636660166aa5574449cf2b
Brian Yang <brian.yang328@gmail.com>
2015-08-31 13:11:29 -0400

    Fix file paths

--- src/PathSumI.java
+++ src/PathSumI.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
  */
 public class PathSumI {
 	public static void main(String[] args) throws IOException{
-		Scanner input = new Scanner(new File("pascal.txt"));
+		Scanner input = new Scanner(new File("/home/brian/workspace/Project-Euler/assets/pascal.txt"));
 
 		ArrayList<ArrayList<Integer>> triangle = new ArrayList<ArrayList<Integer>>();
 
@@ -55,22 +55,3 @@ public class PathSumI {
 	}
 
 }
-
-/**
-pascal.txt contents:
-75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
- */
