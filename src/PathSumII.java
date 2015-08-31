9d87dcc5bb25f3e1401d4b73c969fd53da4cfd8e
Brian Yang <brian.yang328@gmail.com>
2015-08-31 13:13:35 -0400

    Solve Path Sum II #67

---
+++ src/PathSumII.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class PathSumII {
+
+	public static void main(String[] args) throws IOException{
+		Scanner input = new Scanner(new File("/home/brian/workspace/Project-Euler/assets/triangle.txt"));
+
+		ArrayList<ArrayList<Integer>> triangle = new ArrayList<ArrayList<Integer>>();
+
+		StringTokenizer st;
+		ArrayList<Integer> row = new ArrayList<Integer>();
+
+		while (input.hasNextLine()) {
+			st = new StringTokenizer(input.nextLine());
+			while (st.hasMoreTokens()) {
+				row.add(Integer.parseInt(st.nextToken()));
+			}
+			triangle.add(row);
+			row = new ArrayList<Integer>();
+		}
+
+		int indexTriangle = triangle.size() - 1;
+
+		while (indexTriangle - 1 >= 0) {
+			triangle.add(indexTriangle - 1, compareRows(triangle.get(indexTriangle -1), triangle.get(indexTriangle)));
+			triangle.remove(indexTriangle);
+			indexTriangle--;
+		}
+		System.out.println(triangle);
+
+		input.close();
+	}
+	public static ArrayList<Integer> compareRows(ArrayList<Integer> rowA, ArrayList<Integer> rowB) {
+
+		int indexA = 0;
+		int indexB = 0;
+
+		while (indexA < rowA.size()) {
+			int inc = Math.max(rowB.get(indexB), rowB.get(indexB + 1));
+			int newElement = rowA.get(indexA) + inc;
+			rowA.remove(indexA);
+			rowA.add(indexA, newElement);
+			indexA++;
+			indexB++;
+		}
+
+		return rowA;
+	}
+
+}
