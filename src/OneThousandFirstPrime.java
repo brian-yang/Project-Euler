93688fad76e3cb78cfd6a52f02f4e50beb6a2ce8
Brian Yang <brian.yang328@gmail.com>
2015-09-06 19:40:52 -0400

    Solve 10001st Prime #7

---
+++ src/OneThousandFirstPrime.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OneThousandFirstPrime {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(findPrime(10001));
+	}
+
+	/* Sieve of Eratosthenes (modified by ABCD to find 10001st prime
+	 * and translated from Python to Java)
+	 * Python Code by David Eppstein, UC Irvine, 28 Feb 2002
+	 * http://code.activestate.com/recipes/117119/
+	 */
+
+	public static long findPrime(int n) {
+		Map<Long, List<Long>> primes = new HashMap<Long, List<Long>>();
+		List<Long> multiples = new ArrayList<Long>();
+		long curP = 2;
+		int counter = 1;
+
+		while (counter <= n) {
+			if (!primes.containsKey(curP)) {
+				multiples = new ArrayList<Long>();
+				multiples.add(curP);
+				counter++;
+				primes.put(curP * curP, multiples);
+			} else {
+				for (long sp : primes.get(curP)) {
+					if (!primes.containsKey(sp + curP)) { // workaround for Python dict's setdefault()
+						primes.put(sp + curP, new ArrayList<Long>());
+					}
+					primes.get(sp + curP).add(sp);
+				}
+				primes.remove(curP);
+			}
+			curP += 1;
+		}
+
+		/* curP's value after computation is one greater than the expected value
+		 * due to curP being incremented at the end of the while loop
+		 * so what should be returned is curP - 1
+		 */
+		return curP - 1;
+	}
+
+}
