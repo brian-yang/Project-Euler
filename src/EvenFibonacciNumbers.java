874da7917712764212ba03b70ec95306371f7d5f
Brian Yang <brian.yang328@gmail.com>
2015-09-07 10:57:41 -0400

    Solve Even Fibonacci Numbers #2

---
+++ src/EvenFibonacciNumbers.java
@@ -0,0 +1,24 @@
+
+public class EvenFibonacciNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(evenFibs(4_000_000));
+	}
+
+	public static long evenFibs(int limit) {
+		int index = 3;
+		long sum = 0;
+		while (binetFormula(index) <= limit) {
+			sum +=  binetFormula(index);
+			index += 3;
+		}
+		return sum;
+	}
+	public static long binetFormula(int n) {
+		Double phi = (1 + Math.sqrt(5)) / 2;
+		return Math.round(Math.pow(phi, n) / Math.sqrt(5));
+	}
+}
