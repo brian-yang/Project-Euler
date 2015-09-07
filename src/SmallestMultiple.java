7e2b3c291901441afc683adda8ce1b5afaa4c322
Brian Yang <brian.yang328@gmail.com>
2015-09-06 20:19:24 -0400

    Solve Smallest Multiple #5

---
+++ src/SmallestMultiple.java
@@ -0,0 +1,14 @@
+
+public class SmallestMultiple {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Find the product all prime #s from 1 - 20 and multiply
+		// that product by 2's and 3's such that the resulting product
+		// is also divisible by powers of 2 and 3
+		System.out.println(16 * 9 * 5 * 7 * 11 * 13 * 17 * 19);
+	}
+
+}
