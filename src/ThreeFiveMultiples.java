4e3a7c8c7372399336c833636843f929bf1d83a1
Brian Yang <brian.yang328@gmail.com>
2015-09-08 11:33:23 -0400

    Solve Multiples of 3 and 5 #1

---
+++ src/ThreeFiveMultiples.java
@@ -0,0 +1,28 @@
+
+public class ThreeFiveMultiples {
+
+	private static final int LIMIT = 1000;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int threeMultiples = calculateSumMultiples(3, LIMIT);
+		int fiveMultiples = calculateSumMultiples(5, LIMIT);
+		int fifteeenMultiples = calculateSumMultiples(15, LIMIT);
+		System.out.println(threeMultiples + fiveMultiples - fifteeenMultiples);
+
+	}
+	public static int calculateSumMultiples(int n, int limit) {
+		int highQuotient;
+
+		if (limit % n != 0) {
+			limit -= limit % n;
+			highQuotient = limit / n;
+		} else {
+			highQuotient = limit / n - 1;
+		}
+		return n * (highQuotient + 1) * highQuotient / 2;
+	}
+
+}
