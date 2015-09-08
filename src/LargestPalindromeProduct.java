a26d6e2ffc27ee6b999b9ae25abcae2fba8b86d9
Brian Yang <brian.yang328@gmail.com>
2015-09-07 23:17:31 -0400

    Solve Largest Palindrome Product #4

---
+++ src/LargestPalindromeProduct.java
@@ -0,0 +1,40 @@
+public class LargestPalindromeProduct {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(greatestPalindrome());
+	}
+
+	public static int greatestPalindrome() {
+		int product = 1;
+
+		for (int a = 999; a >= 100; a--) {
+			for (int b = 990; b >= 100; b -= 11) {
+				if (isPalindrome(a * b) && a * b > product) {
+					product = a * b;
+				}
+			}
+		}
+		return product;
+	}
+
+	public static boolean isPalindrome(int n) {
+		String half1;
+		StringBuilder half2;
+		String number = Integer.toString(n);
+
+		half1 = number.substring(0, number.length() / 2);
+		if (number.length() % 2 == 0) {
+			half2 = new StringBuilder(number.substring(number.length() / 2, number.length()));
+			half2.reverse();
+		} else {
+			half2 = new StringBuilder(number.substring(number.length() / 2 + 1, number.length()));
+			half2.reverse();
+		}
+
+		return half1.equals(half2.toString());
+	}
+
+}
